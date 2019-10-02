200f4ad3f657b3172e1c7d7e8c639a9adb4880bc
Wigington, Adam <awigington@epri.com>
2019-10-01 18:20:41 -0700

    Added custom bdist_wheel_compiled and bdist_egg_compiled commands to setup for compiled distribution

--- setup.py
+++ setup.py
@@ -7,9 +7,54 @@
 #*****************************************************#
 
 import os
+import sys
 from setuptools import setup, find_packages
+import py_compile
+from distutils import log
+from setuptools.command.build_py import build_py
+from setuptools.command.bdist_egg import bdist_egg
+from wheel.bdist_wheel import bdist_wheel
 
 exec(open(os.path.join('gridopt', 'version.py')).read())
+
+
+# Custom distribution build commands
+class bdist_wheel_compiled(bdist_wheel):
+    """Small customizations to build compiled only wheel."""
+    description = 'build compiled wheel distribution'
+
+
+class bdist_egg_compiled(bdist_egg):
+    """Small customizations to build compiled only egg."""
+    description = 'build compiled egg distribution'
+
+
+if len(sys.argv) > 1 and 'compiled' in sys.argv[1]:
+
+    class build_py(build_py):
+        """
+        A custom build_py command to exclude source files from packaging and
+        include compiled pyc files instead.
+        """
+        def byte_compile(self, files):
+            for file in files:
+                full_path = os.path.abspath(file)
+                if file.endswith('.py'):
+                    log.info("{}  compiling and unlinking".format(file))
+                    py_compile.compile(file, cfile=file+'c')
+                    os.unlink(file)
+                elif file.endswith('pyx') or file.endswith('pxd'):
+                    log.info("{}  unlinking".format(file))
+                    os.unlink(file)
+
+    extra_cmd_classes = {'bdist_wheel_compiled': bdist_wheel_compiled,
+                         'bdist_egg_compiled': bdist_egg_compiled,
+                         'build_py': build_py}
+
+else:
+    extra_cmd_classes = {'bdist_wheel_compiled': bdist_wheel_compiled,
+                         'bdist_egg_compiled': bdist_egg_compiled}
+
 
 setup(name='GRIDOPT',
       zip_safe=False,
@@ -19,6 +64,7 @@ setup(name='GRIDOPT',
       author='Tomas Tinoco De Rubira',
       author_email='ttinoco5687@gmail.com',
       license='BSD 2-Clause License',
+      cmdclass=extra_cmd_classes,
       packages=find_packages(),
       entry_points={'console_scripts': ['gridopt=gridopt.scripts.gridopt:main']},
       classifiers=['Development Status :: 5 - Production/Stable',
